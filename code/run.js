2d8d6dc93981ef2bfff13fa8778c6361d7f4fac7
foxshared <foxshared99@gmail.com>
2021-01-08 20:51:04 +0800

    change back

--- code/run.js
+++ code/run.js
@@ -17,23 +17,13 @@ const { pathfinder, Movements } = require('../')
 
 const { GoalNear, GoalBlock, GoalXZ, GoalY, GoalInvert, GoalFollow } = require('../').goals
 
-const goals = require('../lib/goals');
-
-// Start website port.
-const cool = require('cool-ascii-faces');
 const express = require('express');
-const path = require('path');
-const PORT = process.env.PORT || 5000;
 
-express()
-    .use(express.static(path.join(__dirname, 'public')))
-    .set('views', path.join(__dirname, 'views'))
-    .set('view engine', 'ejs')
-    .get('/', (req, res) => res.render('pages/index'))
-    .get('/cool', (req, res) => res.send(cool()))
-    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+const goals = require('../lib/goals');
+// Start listen port.
+const PORT = process.env.PORT || 3000;
 
-
+const app = express();
 
 // Set the server target and bot username.....cracked only
 var options = {
@@ -202,16 +192,16 @@ function fish_start() {
             fishfix = true
         }, 1000)
     }
-
+    
 }
-function f() {
+function f(){
     bot.fish((err) => {
         fishfix = false
-
+  
         if (err) {
-            bot.chat(err.message)
+          bot.chat(err.message)
         }
-    })
+      })
 }
 
 function sound_detect(sound, cat) {
